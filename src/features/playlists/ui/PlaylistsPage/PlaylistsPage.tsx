b938a49e765cfd72a37059c0bb68068963ff1f51
Anna <blinova.anneta@gmail.com>
2025-08-28 13:12:26 +0200

    `Update PlaylistsPage component to fetch playlists data using useFetchPlaylistsQuery hook and display playlist information`

--- src/features/playlists/ui/PlaylistsPage/PlaylistsPage.tsx
+++ src/features/playlists/ui/PlaylistsPage/PlaylistsPage.tsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import { useFetchPlaylistsQuery } from '../../api/playlistsApi'
+import s from './PlaylistsPage.module.css'
 
 export const PlaylistsPage = () => {
+
+  const {data, isLoading} = useFetchPlaylistsQuery()
+
+  if(isLoading) return <p>Loading...</p>
+
   return (
-    <div>PlaylistsPage</div>
+    <div className={s.container}>
+      <h1>Playlists page</h1>
+      <div className={s.items}>
+        {data?.data.map(playlist => {
+          return (
+            <div className={s.item} key={playlist.id}>
+              <div>title: {playlist.attributes.title}</div>
+              <div>description: {playlist.attributes.description}</div>
+              <div>userName: {playlist.attributes.user.name}</div>
+            </div>
+          )
+        })}
+      </div>
+    </div>
   )
 }
