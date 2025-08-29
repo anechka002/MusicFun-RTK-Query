c6abf52af58c9f4af1acc2998210bf63773fa1f3
Anna <blinova.anneta@gmail.com>
2025-08-29 13:06:41 +0200

    `Refactor playlistsApi to use baseApi` Code Splitting

--- src/features/playlists/api/playlistsApi.ts
+++ src/features/playlists/api/playlistsApi.ts
@@ -1,30 +1,8 @@
+import { baseApi } from '@/app/api/baseApi'
 import type { CreatePlaylistArgs, PlaylistData, PlaylistsResponse, UpdatePlaylistArgs } from './playlistsApi.types'
-// Во избежание ошибок импорт должен быть из `@reduxjs/toolkit/query/react`
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
-// `createApi` - функция из `RTK Query`, позволяющая создать объект `API`
-// для взаимодействия с внешними `API` и управления состоянием приложения
-export const playlistsApi = createApi({
-  // `reducerPath` - имя куда будут сохранены состояние и экшены для этого `API`
-  reducerPath: 'playlistsApi',
-  // `baseQuery` - конфигурация для `HTTP-клиента`, который будет использоваться для отправки запросов
-  baseQuery: fetchBaseQuery({
-    baseUrl: import.meta.env.VITE_BASE_URL,
-    headers: {
-      'API-KEY': import.meta.env.VITE_API_KEY,
-    },
-    prepareHeaders: headers => {
-      headers.set('Authorization', `Bearer ${import.meta.env.VITE_ACCESS_TOKEN}`)
-      return headers
-    },
-  }),
-  // `endpoints` - метод, возвращающий объект с эндпоинтами для `API`, описанными
-  // с помощью функций, которые будут вызываться при вызове соответствующих методов `API`
-  // (например `get`, `post`, `put`, `patch`, `delete`)
-  tagTypes: ['Playlist'],
+export const playlistsApi = baseApi.injectEndpoints({
   endpoints: build => ({
-    // Типизация аргументов (<возвращаемый тип, тип query аргументов (`QueryArg`)>)
-    // `query` по умолчанию создает запрос `get` и указание метода необязательно
     fetchPlaylists: build.query<PlaylistsResponse, void>({
       query: () => `playlists`,
       providesTags: ['Playlist'],
@@ -55,6 +33,4 @@ export const playlistsApi = createApi({
   }),
 })
 
-// `createApi` создает объект `API`, который содержит все эндпоинты в виде хуков,
-// определенные в свойстве `endpoints`
 export const { useFetchPlaylistsQuery, useCreatePlaylistMutation, useRemovePlaylistMutation, useUpdatePlaylistMutation } = playlistsApi
