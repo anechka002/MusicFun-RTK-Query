05c5fc49ae8f6b484583e68743b4b2505f6883f2
Anna <blinova.anneta@gmail.com>
2025-09-04 14:56:00 +0200

    `feat: implement infinite scrolling for tracks page with API calls`

--- src/features/tracks/ui/TracksPage/TracksPage.tsx
+++ src/features/tracks/ui/TracksPage/TracksPage.tsx
@@ -1,7 +1,48 @@
-import React from 'react'
+import {useFetchTracksInfiniteQuery} from "@/features/tracks/api/tracksApi.ts";
+import s from './TracksPage.module.css'
 
 export const TracksPage = () => {
+
+  const {data, hasNextPage, isFetching, isFetchingNextPage, isLoading, fetchNextPage} = useFetchTracksInfiniteQuery()
+
+  const pages = data?.pages.map(page => page.data).flat() || []
+  // const pages = data?.pages.flatMap((page) => page.data) || []
+
+  const loadMoreHandler = () => {
+    if(hasNextPage && !isFetching) {
+      fetchNextPage()
+    }
+  }
+
   return (
-    <div>TracksPage</div>
+    <div>
+      <h1>Tracks page</h1>
+      <div className={s.list}>
+        {pages.map(track => {
+          const { title, user, attachments } = track.attributes
+
+          return (
+            <div key={track.id} className={s.item}>
+              <div>
+                <p>Title: {title}</p>
+                <p>Name: {user.name}</p>
+              </div>
+              {attachments.length ? <audio controls src={attachments[0].url} /> : 'no file'}
+            </div>
+          )
+        })}
+      </div>
+      { !isLoading &&
+        <>
+          {hasNextPage ? (
+            <button onClick={loadMoreHandler} disabled={isFetching}>
+              {isFetchingNextPage ? 'Loading...' : 'Load More'}
+            </button>
+          ) : (
+            <p>Nothing more to load</p>
+          )}
+        </>
+      }
+    </div>
   )
 }
